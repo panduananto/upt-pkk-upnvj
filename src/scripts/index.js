0e25d73162d6b7c56116aaa24e8c3f583934c190
Pandu Ananto <pandunih@gmail.com>
2020-08-09 00:34:27 +0700

    add carousel on home page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,3 +7,45 @@ excerpt.forEach((element) => {
     element.innerHTML = trimmedText;
   }
 });
+
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
+const prevSlideButton = document.querySelector("#prevSlideButton");
+const nextSlideButton = document.querySelector("#nextSlideButton");
+
+let imageCounter = 1;
+const imageWidth = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform =
+  "translateX(" + -imageWidth * imageCounter + "px)";
+
+nextSlideButton.addEventListener("click", () => {
+  if (imageCounter >= carouselImages.length - 1) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  imageCounter++;
+  carouselSlide.style.transform =
+    "translateX(" + -imageWidth * imageCounter + "px)";
+});
+
+prevSlideButton.addEventListener("click", () => {
+  if (imageCounter <= 0) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  imageCounter--;
+  carouselSlide.style.transform =
+    "translateX(" + -imageWidth * imageCounter + "px)";
+});
+
+carouselSlide.addEventListener("transitionend", () => {
+  if (carouselImages[imageCounter].id === "lastClone") {
+    carouselSlide.style.transition = "none";
+    imageCounter = carouselImages.length - 2;
+    carouselSlide.style.transform =
+      "translateX(" + -imageWidth * imageCounter + "px)";
+  }
+  if (carouselImages[imageCounter].id === "firstClone") {
+    carouselSlide.style.transition = "none";
+    imageCounter = carouselImages.length - imageCounter;
+    carouselSlide.style.transform =
+      "translateX(" + -imageWidth * imageCounter + "px)";
+  }
+});
